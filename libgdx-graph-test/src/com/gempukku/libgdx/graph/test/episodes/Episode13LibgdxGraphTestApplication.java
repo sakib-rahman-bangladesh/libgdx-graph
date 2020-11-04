6dd393c40b9a0133646f84822f611ffa20fbca00
marcin.sciesinski <marcin.sciesinski>
2020-11-04 11:44:49 -0800

    Preparing for episode 13

---
+++ libgdx-graph-test/src/com/gempukku/libgdx/graph/test/episodes/Episode13LibgdxGraphTestApplication.java
@@ -0,0 +1,223 @@
+package com.gempukku.libgdx.graph.test.episodes;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cubemap;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.TextureArray;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.graphics.glutils.GLFrameBuffer;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.gempukku.libgdx.graph.GraphLoader;
+import com.gempukku.libgdx.graph.pipeline.PipelineLoaderCallback;
+import com.gempukku.libgdx.graph.pipeline.PipelineRenderer;
+import com.gempukku.libgdx.graph.pipeline.RenderOutputs;
+import com.gempukku.libgdx.graph.shader.environment.GraphShaderEnvironment;
+import com.gempukku.libgdx.graph.shader.models.GraphShaderModels;
+import com.gempukku.libgdx.graph.shader.models.Transforms;
+import com.gempukku.libgdx.graph.test.WhitePixel;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Episode13LibgdxGraphTestApplication extends ApplicationAdapter {
+    private long lastProcessedInput;
+
+    private Array<Disposable> disposables = new Array<>();
+    private PipelineRenderer pipelineRenderer;
+
+    private Camera camera;
+    private Stage stage;
+    private GraphShaderEnvironment lights;
+
+    private Model tiledWall;
+    private Model burner;
+    private Model cylinder;
+
+    @Override
+    public void create() {
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+        WhitePixel.initialize();
+
+        lights = createLights();
+        stage = createStage();
+        disposables.add(stage);
+
+        camera = createCamera();
+
+        pipelineRenderer = loadPipelineRenderer();
+        createModels(pipelineRenderer.getGraphShaderModels());
+
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    private GraphShaderEnvironment createLights() {
+        float ambientBrightness = 0.3f;
+        float directionalBrightness = 0.8f;
+        GraphShaderEnvironment lights = new GraphShaderEnvironment();
+        lights.setAmbientColor(new Color(ambientBrightness, ambientBrightness, ambientBrightness, 1f));
+        DirectionalLight directionalLight = new DirectionalLight();
+        directionalLight.setColor(directionalBrightness, directionalBrightness, directionalBrightness, 1f);
+        directionalLight.setDirection(-0.3f, -0.4f, -1);
+        lights.addDirectionalLight(directionalLight);
+        return lights;
+    }
+
+    private Camera createCamera() {
+        PerspectiveCamera camera = new PerspectiveCamera();
+        camera.near = 0.5f;
+        camera.far = 100f;
+
+        camera.position.set(15, 4, 0);
+        camera.up.set(0f, 1f, 0f);
+        camera.lookAt(0, 3, 0f);
+        camera.update();
+
+        return camera;
+    }
+
+    private void createModels(GraphShaderModels models) {
+        ModelBuilder modelBuilder = new ModelBuilder();
+
+        float x = -5f;
+        tiledWall = modelBuilder.createRect(
+                x, 20, 10,
+                x, 0, 10,
+                x, 0, -10,
+                x, 20, -10,
+                1, 0, 0,
+                new Material(), VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates);
+        disposables.add(tiledWall);
+
+        float y = 0f;
+        burner = modelBuilder.createRect(
+                x, y, 5,
+                -x, y, 5,
+                -x, y, -5,
+                x, y, -5,
+                0, 1, 0,
+                new Material(), VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates);
+        disposables.add(burner);
+
+        float cylinderHeight = 8f;
+        float cylinderSize = 7f;
+        cylinder = modelBuilder.createCylinder(cylinderSize, cylinderHeight, cylinderSize, 50,
+                new Material(), VertexAttributes.Usage.Position | VertexAttributes.Usage.TextureCoordinates | VertexAttributes.Usage.Normal);
+        disposables.add(cylinder);
+
+        registerModels(models);
+    }
+
+    private void registerModels(GraphShaderModels models) {
+        float cylinderHeight = 8f;
+        String tiledWallId = models.registerModel(tiledWall);
+        models.addModelDefaultTag(tiledWallId, "tiled-wall");
+        models.createModelInstance(tiledWallId);
+
+        String burnerId = models.registerModel(burner);
+        models.addModelDefaultTag(burnerId, "burner");
+        models.createModelInstance(burnerId);
+
+        String cylinderId = models.registerModel(cylinder);
+        models.addModelDefaultTag(cylinderId, "heat-displacement");
+        String cylinderInstanceId = models.createModelInstance(cylinderId);
+        models.updateTransform(cylinderInstanceId, Transforms.create().translate(0, 0.05f + cylinderHeight / 2f, 0f));
+    }
+
+    private Stage createStage() {
+        Skin skin = new Skin(Gdx.files.classpath("skin/default/uiskin.json"));
+        disposables.add(skin);
+
+        Stage stage = new Stage(new ScreenViewport());
+
+        Table tbl = new Table(skin);
+
+        tbl.setFillParent(true);
+        tbl.align(Align.topLeft);
+
+        stage.addActor(tbl);
+        return stage;
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void render() {
+        float delta = Gdx.graphics.getDeltaTime();
+
+        reloadRendererIfNeeded();
+        stage.act(delta);
+
+        pipelineRenderer.render(delta, RenderOutputs.drawToScreen);
+    }
+
+    private void reloadRendererIfNeeded() {
+        long currentTime = System.currentTimeMillis();
+        if (lastProcessedInput + 200 < currentTime) {
+            if (Gdx.input.isKeyPressed(Input.Keys.R)) {
+                lastProcessedInput = currentTime;
+                pipelineRenderer.dispose();
+                pipelineRenderer = loadPipelineRenderer();
+                registerModels(pipelineRenderer.getGraphShaderModels());
+            }
+        }
+    }
+
+    @Override
+    public void dispose() {
+        for (Disposable disposable : disposables) {
+            disposable.dispose();
+        }
+        pipelineRenderer.dispose();
+        WhitePixel.dispose();
+
+        Gdx.app.debug("Unclosed", Cubemap.getManagedStatus());
+        Gdx.app.debug("Unclosed", GLFrameBuffer.getManagedStatus());
+        Gdx.app.debug("Unclosed", Mesh.getManagedStatus());
+        Gdx.app.debug("Unclosed", Texture.getManagedStatus());
+        Gdx.app.debug("Unclosed", TextureArray.getManagedStatus());
+        Gdx.app.debug("Unclosed", ShaderProgram.getManagedStatus());
+    }
+
+    private PipelineRenderer loadPipelineRenderer() {
+        try {
+            InputStream stream = Gdx.files.local("episodes/episode13.json").read();
+            try {
+                PipelineRenderer pipelineRenderer = GraphLoader.loadGraph(stream, new PipelineLoaderCallback());
+                setupPipeline(pipelineRenderer);
+                return pipelineRenderer;
+            } finally {
+                stream.close();
+            }
+        } catch (IOException exp) {
+            throw new RuntimeException("Unable to load pipeline", exp);
+        }
+    }
+
+    private void setupPipeline(PipelineRenderer pipelineRenderer) {
+        pipelineRenderer.setPipelineProperty("Camera", camera);
+        pipelineRenderer.setPipelineProperty("Lights", lights);
+        pipelineRenderer.setPipelineProperty("Stage", stage);
+    }
+}
