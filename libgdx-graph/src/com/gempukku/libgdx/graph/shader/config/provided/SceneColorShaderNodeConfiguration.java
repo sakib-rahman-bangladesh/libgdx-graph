6cd636d2e42027e3d8b721be728b631634fb6a82
marcin.sciesinski <marcin.sciesinski>
2020-11-13 02:27:06 -0800

    Making scene color into texture node

--- libgdx-graph/src/com/gempukku/libgdx/graph/shader/config/provided/SceneColorShaderNodeConfiguration.java
+++ libgdx-graph/src/com/gempukku/libgdx/graph/shader/config/provided/SceneColorShaderNodeConfiguration.java
@@ -1,16 +1,13 @@
 package com.gempukku.libgdx.graph.shader.config.provided;
 
 import com.gempukku.libgdx.graph.NodeConfigurationImpl;
-import com.gempukku.libgdx.graph.pipeline.loader.node.GraphNodeInputImpl;
 import com.gempukku.libgdx.graph.pipeline.loader.node.GraphNodeOutputImpl;
 import com.gempukku.libgdx.graph.shader.ShaderFieldType;
 
 public class SceneColorShaderNodeConfiguration extends NodeConfigurationImpl<ShaderFieldType> {
     public SceneColorShaderNodeConfiguration() {
         super("SceneColor", "Scene color", "Provided");
-        addNodeInput(
-                new GraphNodeInputImpl<ShaderFieldType>("screenPosition", "Screen position", ShaderFieldType.Vector2));
         addNodeOutput(
-                new GraphNodeOutputImpl<ShaderFieldType>("color", "Color", ShaderFieldType.Color));
+                new GraphNodeOutputImpl<ShaderFieldType>("texture", "Texture", ShaderFieldType.TextureRegion));
     }
 }
--- libgdx-graph/src/com/gempukku/libgdx/graph/shader/node/provided/SceneColorShaderNodeBuilder.java
+++ libgdx-graph/src/com/gempukku/libgdx/graph/shader/node/provided/SceneColorShaderNodeBuilder.java
@@ -1,16 +1,23 @@
 package com.gempukku.libgdx.graph.shader.node.provided;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g3d.Renderable;
 import com.badlogic.gdx.utils.JsonValue;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.ObjectSet;
 import com.gempukku.libgdx.graph.LibGDXCollections;
+import com.gempukku.libgdx.graph.WhitePixel;
+import com.gempukku.libgdx.graph.shader.BasicShader;
 import com.gempukku.libgdx.graph.shader.GraphShader;
 import com.gempukku.libgdx.graph.shader.GraphShaderContext;
+import com.gempukku.libgdx.graph.shader.ShaderContext;
 import com.gempukku.libgdx.graph.shader.ShaderFieldType;
+import com.gempukku.libgdx.graph.shader.UniformRegistry;
 import com.gempukku.libgdx.graph.shader.UniformSetters;
 import com.gempukku.libgdx.graph.shader.builder.CommonShaderBuilder;
 import com.gempukku.libgdx.graph.shader.builder.VertexShaderBuilder;
 import com.gempukku.libgdx.graph.shader.config.provided.SceneColorShaderNodeConfiguration;
+import com.gempukku.libgdx.graph.shader.models.impl.GraphShaderModelInstance;
 import com.gempukku.libgdx.graph.shader.node.ConfigurationCommonShaderNodeBuilder;
 import com.gempukku.libgdx.graph.shader.node.DefaultFieldOutput;
 
@@ -28,17 +35,37 @@ public class SceneColorShaderNodeBuilder extends ConfigurationCommonShaderNodeBu
     protected ObjectMap<String, ? extends FieldOutput> buildCommonNode(boolean designTime, String nodeId, JsonValue data, ObjectMap<String, FieldOutput> inputs, ObjectSet<String> producedOutputs,
                                                                        CommonShaderBuilder commonShaderBuilder, GraphShaderContext graphShaderContext, GraphShader graphShader) {
         graphShader.setUsingColorTexture(true);
+        String textureName = "u_" + nodeId;
+        String transformName = "u_UV" + nodeId;
         if (designTime) {
-            return LibGDXCollections.singletonMap("color", new DefaultFieldOutput(ShaderFieldType.Color, "vec4(1.0)"));
-        } else {
-            commonShaderBuilder.addUniformVariable("u_sceneColorTexture", "sampler2D", true, UniformSetters.colorTexture);
+            Texture texture = WhitePixel.sharedInstance.texture;
 
-            FieldOutput screenPosition = inputs.get("screenPosition");
-            String screenPositionValue = screenPosition != null ? screenPosition.getRepresentation() : "gl_FragCoord";
-            String name = "depth_" + nodeId;
-            commonShaderBuilder.addMainLine("// Scene color node");
-            commonShaderBuilder.addMainLine("vec4 " + name + " = texture2D(u_sceneColorTexture, " + screenPositionValue + ".xy);");
-            return LibGDXCollections.singletonMap("color", new DefaultFieldOutput(ShaderFieldType.Color, name));
+            final Texture finalTexture = texture;
+            commonShaderBuilder.addUniformVariable(textureName, "sampler2D", false,
+                    new UniformRegistry.UniformSetter() {
+                        @Override
+                        public void set(BasicShader shader, int location, ShaderContext shaderContext, GraphShaderModelInstance graphShaderModelInstance, Renderable renderable) {
+                            shader.setUniform(location, finalTexture);
+                        }
+                    });
+            commonShaderBuilder.addUniformVariable(transformName, "vec4", false,
+                    new UniformRegistry.UniformSetter() {
+                        @Override
+                        public void set(BasicShader shader, int location, ShaderContext shaderContext, GraphShaderModelInstance graphShaderModelInstance, Renderable renderable) {
+                            shader.setUniform(location, 0f, 0f, 1f, 1f);
+                        }
+                    });
+        } else {
+            commonShaderBuilder.addUniformVariable(textureName, "sampler2D", true, UniformSetters.colorTexture);
+            commonShaderBuilder.addUniformVariable(transformName, "vec4", false,
+                    new UniformRegistry.UniformSetter() {
+                        @Override
+                        public void set(BasicShader shader, int location, ShaderContext shaderContext, GraphShaderModelInstance graphShaderModelInstance, Renderable renderable) {
+                            shader.setUniform(location, 0f, 0f, 1f, 1f);
+                        }
+                    });
         }
+
+        return LibGDXCollections.singletonMap("texture", new DefaultFieldOutput(ShaderFieldType.TextureRegion, transformName, textureName));
     }
 }
