a2ba79cdb270149264bcfd33d5222fc899bc895c
marcin.sciesinski <marcin.sciesinski>
2020-12-11 02:14:26 -0800

    Working on particles

--- libgdx-graph-test/src/com/gempukku/libgdx/graph/test/TestScene.java
+++ libgdx-graph-test/src/com/gempukku/libgdx/graph/test/TestScene.java
@@ -3,29 +3,21 @@ package com.gempukku.libgdx.graph.test;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.FPSLogger;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.g3d.Model;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
-import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader;
-import com.badlogic.gdx.graphics.g3d.utils.AnimationController;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Align;
-import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.gempukku.libgdx.graph.loader.GraphLoader;
 import com.gempukku.libgdx.graph.pipeline.PipelineLoaderCallback;
 import com.gempukku.libgdx.graph.pipeline.PipelineRenderer;
 import com.gempukku.libgdx.graph.pipeline.RenderOutputs;
-import com.gempukku.libgdx.graph.shader.Transforms;
 import com.gempukku.libgdx.graph.shader.environment.GraphShaderEnvironment;
-import com.gempukku.libgdx.graph.shader.model.GraphShaderModels;
+import com.gempukku.libgdx.graph.shader.particles.GraphParticleEffects;
+import com.gempukku.libgdx.graph.shader.particles.generator.PointParticleGenerator;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -36,16 +28,11 @@ public class TestScene implements LibgdxGraphTestScene {
     private Stage stage;
     private Skin skin;
     private GraphShaderEnvironment lights;
-    private float cameraSpeed = -0.8f;
-    private float cameraAngle = 0f;
-    private float cameraDistance = 4f;
-    private AnimationController animationController;
-
-    private Model model;
-    private String mainRobot;
+    private FPSLogger fpsLogger = new FPSLogger();
 
     @Override
     public void initializeScene() {
+
         WhitePixel.initialize();
 
         lights = createLights();
@@ -54,13 +41,13 @@ public class TestScene implements LibgdxGraphTestScene {
         camera = createCamera();
 
         pipelineRenderer = loadPipelineRenderer();
-        createModels(pipelineRenderer.getGraphShaderModels());
+        createParticleEffects(pipelineRenderer.getGraphParticleEffects());
 
         Gdx.input.setInputProcessor(stage);
     }
 
     private GraphShaderEnvironment createLights() {
-        float ambientBrightness = 0.3f;
+        float ambientBrightness = 0.8f;
         float directionalBrightness = 0.8f;
         GraphShaderEnvironment lights = new GraphShaderEnvironment();
         lights.setAmbientColor(new Color(ambientBrightness, ambientBrightness, ambientBrightness, 1f));
@@ -73,60 +60,29 @@ public class TestScene implements LibgdxGraphTestScene {
 
     private Camera createCamera() {
         PerspectiveCamera camera = new PerspectiveCamera();
-        camera.near = 0.5f;
-        camera.far = 100f;
+        camera.near = 0.1f;
+        camera.far = 100;
+        camera.position.set(0, 7, 5);
+        camera.up.set(0f, 1f, 0f);
+        camera.lookAt(0, 4, 0f);
+        camera.update();
+
         return camera;
     }
 
-    private void createModels(GraphShaderModels models) {
-        JsonReader jsonReader = new JsonReader();
-        G3dModelLoader modelLoader = new G3dModelLoader(jsonReader);
-        model = modelLoader.loadModel(Gdx.files.classpath("model/gold-robot/gold-robot.g3dj"));
-
-        String modelId = models.registerModel(model);
-        models.addModelDefaultTag(modelId, "Default");
-        final float scale = 0.008f;
-
-        mainRobot = models.createModelInstance(modelId);
-        models.updateTransform(mainRobot, Transforms.create().idt().scale(scale, scale, scale));
-        animationController = models.createAnimationController(mainRobot);
-        animationController.animate("Root|jog", -1, null, 0f);
-
-        String secondRobot = models.createModelInstance(modelId);
-        models.updateTransform(secondRobot, Transforms.create().idt().translate(1.5f, 0, 0).scale(scale, scale, scale));
+    private void createParticleEffects(GraphParticleEffects effects) {
+        PointParticleGenerator particleGenerator = new PointParticleGenerator(10f);
+        particleGenerator.getLocation().set(0, 0, 0);
+        String effectId = effects.createEffect("effect", particleGenerator);
+        effects.startEffect(effectId);
     }
 
     private Stage createStage() {
         skin = new Skin(Gdx.files.classpath("skin/default/uiskin.json"));
 
-        final CheckBox selected = new CheckBox("Outline", skin);
-        selected.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent event, Actor actor) {
-                        if (selected.isChecked())
-                            pipelineRenderer.getGraphShaderModels().addTag(mainRobot, "Outline");
-                        else
-                            pipelineRenderer.getGraphShaderModels().removeTag(mainRobot, "Outline");
-                    }
-                });
-
-        final Slider outlineWidth = new Slider(0f, 0.3f, 0.001f, false, skin);
-        outlineWidth.setValue(0.01f);
-        outlineWidth.addListener(
-                new ChangeListener() {
-                    @Override
-                    public void changed(ChangeEvent event, Actor actor) {
-                        pipelineRenderer.getGraphShaderModels().setProperty(mainRobot, "Outline Width", outlineWidth.getValue());
-                    }
-                });
-
         Stage stage = new Stage(new ScreenViewport());
 
         Table tbl = new Table(skin);
-        tbl.add(selected).colspan(2).row();
-        tbl.add("Width");
-        tbl.add(outlineWidth).row();
 
         tbl.setFillParent(true);
         tbl.align(Align.topLeft);
@@ -143,14 +99,7 @@ public class TestScene implements LibgdxGraphTestScene {
     @Override
     public void renderScene() {
         float delta = Gdx.graphics.getDeltaTime();
-        animationController.update(delta);
-
-        cameraAngle += delta * cameraSpeed;
-
-        camera.position.set(cameraDistance * MathUtils.sin(cameraAngle), 3f, cameraDistance * MathUtils.cos(cameraAngle));
-        camera.up.set(0f, 1f, 0f);
-        camera.lookAt(0, 0.8f, 0f);
-        camera.update();
+        fpsLogger.log();
 
         stage.act(delta);
 
@@ -159,7 +108,6 @@ public class TestScene implements LibgdxGraphTestScene {
 
     @Override
     public void disposeScene() {
-        model.dispose();
         stage.dispose();
         skin.dispose();
         pipelineRenderer.dispose();
