db2a800e8c6bd2cdd4d7e8b29db40247d782fd7c
marcin.sciesinski <marcin.sciesinski>
2020-12-13 06:47:44 -0800

    Added star shape!

--- libgdx-graph/resources/glsl/fragment/shape/starShape.glsl
+++ libgdx-graph/resources/glsl/fragment/shape/starShape.glsl
@@ -1,14 +1,10 @@
-float getStarValue(vec2 vec, float arms, float min, float max, float power) {
-    vec2 vectorFromMiddle = normalize(vec - vec2(0.5, 0.5));
-    // Arccosine node
+float getStarValue(vec2 uv, float arms, float min, float max, float power) {
+    vec2 vectorFromMiddle = normalize(uv - vec2(0.5));
     float topPartAngle = acos(vectorFromMiddle.x);
-    // Step node
     float isBottomMultiplier = step(0.0, vectorFromMiddle.y);
-    // Arccosine node
     float bottomAngle = acos(-vectorFromMiddle.x);
-    // Add node
     float angle = topPartAngle + isBottomMultiplier * bottomAngle * 2.0;
     float anglePerArm = 2.0 * 3.14159265358979323846 / arms;
     float inArm = mod(angle, anglePerArm) / anglePerArm;
-    return min + (max - min) * pow(2.0 * abs(inArm-0.5), power);
+    return min + (max - min) * pow(2.0 * abs(inArm - 0.5), power);
 }
